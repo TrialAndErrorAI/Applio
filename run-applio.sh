30746121f80b5cb6442d5d39dd8db0a402fa1200
Rajan Balana <rajan.balana@collinsongroup.com>
2024-07-06 15:08:21 +0530

    Updated the run script for applio to restart it if it gets killed for any reason

--- run-applio.sh
+++ run-applio.sh
@@ -6,4 +6,8 @@ printf "\033]0;Applio\007"
  export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
  
 clear
-python app.py --open
+while true; do
+    python app.py
+    echo "Applio Exited. Restarting Applio in 5 seconds..."
+    sleep 5
+done
