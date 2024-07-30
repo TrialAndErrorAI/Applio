cccd1d525118a5ca81295e7dd5c70c205012265a
Rajan Balana <rajan.balana@collinsongroup.com>
2024-07-30 16:50:56 +0530

    scripts added

--- run-applio.sh
+++ run-applio.sh
@@ -5,7 +5,6 @@ printf "\033]0;Applio\007"
  export PYTORCH_ENABLE_MPS_FALLBACK=1
  export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
  
-clear
 while true; do
     python app.py
     echo "Applio Exited. Restarting Applio in 5 seconds..."
